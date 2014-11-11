a5d264cf064f874fb1b312a35b88a73c299936d0
YounSung <infogviolet@gmail.com>
2014-11-11 16:51:59 +0900

    Update FigureCalculation.java

--- src/FigureCalculation.java
+++ src/FigureCalculation.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class FigureCalculation {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 
 		FileReader fr = null;
